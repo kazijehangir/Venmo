864e9b21eebd48e513205d1c86c2ead1cb028ecb
Jehangir Kazi <kazi.jehangir@gmail.com>
2017-10-28 03:19:19 +0500

    fake profiles and proxy added

--- seleniumGetPostsParallel.py
+++ seleniumGetPostsParallel.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.proxy import *
 
 import _thread
 import time
@@ -41,7 +41,7 @@ def getPostInLink(link, driver):
             with printLock:
                 print('Found show more link:', links[i])
                 print('With text:', texts[i])
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(5, 30))
             driver.get(links[i])
             posts.extend(getPostInLink(links[i], driver))
             break
@@ -50,21 +50,27 @@ def getPostInLink(link, driver):
 def getPostInProfile(link, driver):
     posts = []
     try:
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(5, 30))
         driver.get(link)
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(5, 30))
         driver.get(driver.current_url + '&v=timeline')
         outbound = driver.find_elements_by_class_name('i')
         links = [div.find_element_by_css_selector('a').get_attribute('href') for div in outbound[1:]]
         texts = [div.find_element_by_css_selector('a').text for div in outbound[1:]]
-
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        with printLock:
+            print(exc_type, fname, exc_tb.tb_lineno, e)
+        pass
+    try:
         posts = getPostInLink(link, driver)
         for i in range(len(links)):
             if re.match(r'[0-9]{4}', texts[i]):
                 with printLock:
                     print('Found outbound year link:', links[i])
                     print('With text:', texts[i])
-                time.sleep(random.uniform(1, 3))
+                time.sleep(random.uniform(5, 30))
                 driver.get(links[i])
                 posts.extend(getPostInLink(links[i], driver))
     except Exception as e:
@@ -79,30 +85,45 @@ def scrapePosts(i):
     try:
         with printLock:
             print(i, ' Starting thread')
-        driver = webdriver.Firefox(executable_path='/home/jehangir/Documents/geckodriver')
-        # driver = webdriver.Chrome(executable_path='/home/jehangir/Documents/chromedriver')
+
+        proxies = open('proxies.txt', 'r').read().split('\n')
+        
+        proxy = Proxy({
+            'proxyType': ProxyType.MANUAL,
+            'httpProxy': proxies[int(i)],
+            'ftpProxy': proxies[int(i)],
+            'sslProxy': proxies[int(i)],
+            'noProxy': '' # set this value as desired
+            })
+        
+        driver = webdriver.Firefox(executable_path='/home/kazi/Documents/geckodriver')
+        # driver = webdriver.Chrome(executable_path='/home/kazi/Documents/chromedriver')
         driver.get("https://www.facebook.com/")
         with printLock:
             print(i, 'Started driver')
-        time.sleep(random.uniform(1, 3))
+
+        time.sleep(random.uniform(5, 30))
+
+        logins = open('fakeprofiles.txt', 'r').read().split('\n')
+        login = logins[int(i)]
+        email = login.split(',')[0]
+        passw = login.split(',')[1]
 
         inputEmail = driver.find_element_by_id("email")
-        inputEmail.send_keys('blahblahblahchup@gmail.com')
+        inputEmail.send_keys(email)
         inputPass = driver.find_element_by_id("pass")
-        # inputEmail.send_keys(Keys.TAB)
-        # inputPass = driver.switch_to.active_element['value']
+        inputPass.send_keys(passw)
 
-        inputPass.send_keys("ultramantis11")
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(5, 30))
 
         inputPass.submit()
         with printLock:
-            print(i , 'Logged in successfully...')
+            print(i , 'Logged in successfully with', email, passw)
         profiles = open('profiles_' + str(i) + '.txt', 'r').read().split('\n')
         outfile = open('posts_' + str(i) + '.json', 'a')
         done = open('done_' + str(i) + '.txt', 'a')
         for j, prof in enumerate(profiles):
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(5, 30))
             link = "https://mbasic.facebook.com/" + prof.strip()
             with printLock:
                 print(i, 'Scraping profile: ', link)
