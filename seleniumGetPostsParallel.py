6a9ea2909608d9d5e123d63ee1fdfce6e6c8937b
Jehangir Kazi <kazi.jehangir@gmail.com>
2017-11-01 07:30:30 +0500

    updated fake profiles

--- seleniumGetPostsParallel.py
+++ seleniumGetPostsParallel.py
@@ -9,10 +9,69 @@ import _thread
 import time
 import random
 import json
-import sys, os, re
+import sys
+import os
+import re
 import threading
 
 printLock = threading.Lock()
+
+
+def getProxyList():
+    proxies = []
+    driver = webdriver.Firefox(
+        executable_path='/home/jehangir/Documents/geckodriver')
+    driver.get('https://free-proxy-list.net/')
+    odd = driver.find_elements_by_class_name('odd')
+    for row in odd:
+        proxy = row.find_elements_by_tag_name('td')
+        ip = proxy[0].text
+        port = proxy[1].text
+        # print('cell', cell)
+        print('Found proxy:', ip, port)
+        proxies.append(ip + ':' + port)
+    even = driver.find_elements_by_class_name('even')
+    for row in even:
+        proxy = row.find_elements_by_tag_name('td')
+        ip = proxy[0].text
+        port = proxy[1].text
+        # print('cell', cell)
+        print('Found proxy:', ip, port)
+        proxies.append(ip + ':' + port)
+
+    driver.close()
+    print("Got", len(proxies), "proxies.")
+    return proxies
+
+
+def isValid(proxy):
+    ip = proxy.split(':')[0]
+    port = int(proxy.split(':')[1])
+
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference('network.proxy.ssl_port', int(port))
+    fp.set_preference('network.proxy.ssl', ip)
+    fp.set_preference('network.proxy.http_port', int(port))
+    fp.set_preference('network.proxy.http', ip)
+    fp.set_preference('network.proxy.type', 1)
+
+    driver = webdriver.Firefox(
+            executable_path='/home/jehangir/Documents/geckodriver',
+            firefox_profile=fp)
+    try:
+        driver.get("https://whatsmyip.org")
+        div = driver.find_element_by_id('ip')
+        result_ip = div.text
+    except Exception as e:
+        print('Exception: ', e)
+        driver.close()
+        return False
+    driver.close()
+    return result_ip == ip
+
+proxies = [proxy for proxy in getProxyList() if isValid(proxy)]
+print('Working proxies', len(proxies))
+
 
 def getPostInLink(link, driver):
     posts = []
@@ -47,6 +106,7 @@ def getPostInLink(link, driver):
             break
     return posts
 
+
 def getPostInProfile(link, driver):
     posts = []
     try:
@@ -80,32 +140,37 @@ def getPostInProfile(link, driver):
             print(exc_type, fname, exc_tb.tb_lineno, e)
         pass
     return posts
-            
+
+
 def scrapePosts(i):
     try:
         with printLock:
             print(i, ' Starting thread')
 
-        proxies = open('proxies.txt', 'r').read().split('\n')
-        
-        proxy = Proxy({
-            'proxyType': ProxyType.MANUAL,
-            'httpProxy': proxies[int(i)],
-            'ftpProxy': proxies[int(i)],
-            'sslProxy': proxies[int(i)],
-            'noProxy': '' # set this value as desired
-            })
-        
-        driver = webdriver.Firefox(executable_path='/home/kazi/Documents/geckodriver')
-        # driver = webdriver.Chrome(executable_path='/home/kazi/Documents/chromedriver')
+        proxy = proxies[int(i) % len(proxies)]
+        ip = proxy.split(':')[0]
+        port = int(proxy.split(':')[1])
+
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('network.proxy.ssl_port', int(port))
+        fp.set_preference('network.proxy.ssl', ip)
+        fp.set_preference('network.proxy.http_port', int(port))
+        fp.set_preference('network.proxy.http', ip)
+        fp.set_preference('network.proxy.type', 1)
+
+        driver = webdriver.Firefox(
+            executable_path='/home/jehangir/Documents/geckodriver',
+            firefox_profile=fp)
+        # driver = webdriver.Chrome(
+        # executable_path='/home/kazi/Documents/chromedriver')
         driver.get("https://www.facebook.com/")
         with printLock:
-            print(i, 'Started driver')
+            print(i, 'Started driver with proxy', proxy)
 
         time.sleep(random.uniform(5, 30))
 
         logins = open('fakeprofiles.txt', 'r').read().split('\n')
-        login = logins[int(i)]
+        login = logins[int(i) % len(logins)]
         email = login.split(',')[0]
         passw = login.split(',')[1]
 
@@ -118,7 +183,7 @@ def scrapePosts(i):
 
         inputPass.submit()
         with printLock:
-            print(i , 'Logged in successfully with', email, passw)
+            print(i, 'Logged in successfully with', email, passw)
         profiles = open('profiles_' + str(i) + '.txt', 'r').read().split('\n')
         outfile = open('posts_' + str(i) + '.json', 'a')
         done = open('done_' + str(i) + '.txt', 'a')
@@ -128,7 +193,7 @@ def scrapePosts(i):
             with printLock:
                 print(i, 'Scraping profile: ', link)
             posts = getPostInProfile(link, driver)
-            json.dump({prof: posts} , outfile)
+            json.dump({prof: posts}, outfile)
             outfile.write('\n')
             done.write(prof)
             done.write('\n')
@@ -138,7 +203,8 @@ def scrapePosts(i):
                 os.fsync(outfile)
                 os.fsync(done)
             with printLock:
-                print(i, 'Got ' + str(len(posts)) + ' posts for profile ' + prof)
+                print(i, 'Got ' + str(len(posts)) +
+                         ' posts for profile ' + prof)
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -147,7 +213,9 @@ def scrapePosts(i):
         pass
 # scrapePosts(0)
 
-for i in range(5):
+NUM_THREADS = 5
+
+for i in range(NUM_THREADS):
     try:
         t = threading.Thread(target=scrapePosts, args=(i,))
         t.start()
